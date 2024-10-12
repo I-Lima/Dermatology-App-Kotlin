4020c1480ed66afe37898f1f9188675aeb68f79f
Ingrid Lima <ibezerradelima@gmail.com>
2024-10-12 15:39:06 -0300

    :sparkles: feat: Add splash screen
    #1

---
+++ app/src/main/java/br/com/ilstudio/dermatologyapp/ui/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package br.com.ilstudio.dermatologyapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import br.com.ilstudio.dermatologyapp.R
+
+class SplashScreenActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
