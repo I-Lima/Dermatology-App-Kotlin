cf2010d34f8a37478d757cfdb2cdc6014d843671
Ingrid Lima <ibezerradelima@gmail.com>
2024-10-13 11:41:18 -0300

    :wrench: chore: Update minSDK to 26
    #5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "br.com.ilstudio.dermatologyapp"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
