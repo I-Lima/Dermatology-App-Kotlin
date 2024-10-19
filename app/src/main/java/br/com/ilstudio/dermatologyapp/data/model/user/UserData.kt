fcdf1e06fed302550386d963ccedef30e0f28fa4
Ingrid Lima <ibezerradelima@gmail.com>
2024-10-18 22:39:02 -0300

    Add firestore config
    #29

--- app/src/main/java/br/com/ilstudio/dermatologyapp/data/model/user/UserData.kt
+++ app/src/main/java/br/com/ilstudio/dermatologyapp/data/model/user/UserData.kt
@@ -1,15 +1,14 @@
 package br.com.ilstudio.dermatologyapp.data.model.user
 
-import br.com.ilstudio.dermatologyapp.domain.model.User
 import java.sql.Timestamp
 
 data class UserData(
     val uid: String,
     val name: String,
     val email: String,
-    val mobile_number: String,
-    val date_birth: Timestamp,
-    val profile_picture: String?,
-    val created_at: String,
-    val updated_at: String
+    val mobileNumber: String,
+    val dateBirth: Timestamp,
+    val profilePicture: String?,
+    val createdAt: String,
+    val updatedAt: String
 )
