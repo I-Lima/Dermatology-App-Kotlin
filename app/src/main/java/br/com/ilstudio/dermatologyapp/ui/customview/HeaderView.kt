6378fe1f9138264e43ac733f8d1194e05e095352
Ingrid Lima <ibezerradelima@gmail.com>
2024-10-21 22:24:02 -0300

    :sparkles: feat: New account google screen
    #27

--- app/src/main/java/br/com/ilstudio/dermatologyapp/ui/customview/HeaderView.kt
+++ app/src/main/java/br/com/ilstudio/dermatologyapp/ui/customview/HeaderView.kt
@@ -15,16 +15,22 @@ class HeaderView @JvmOverloads constructor (
     defStyleAttr: Int = 0
 ): ConstraintLayout(context, attrs, defStyleAttr) {
     private var title: TextView
+    private var button: ImageView
 
     init {
         LayoutInflater.from(context).inflate(R.layout.view_header, this, true)
         title = findViewById(R.id.title)
+        button = findViewById(R.id.button_back)
 
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.HeaderView, 0, 0)
 
             val labelText = typedArray.getString(R.styleable.HeaderView_title)
             title.text = labelText ?: ""
+
+            val visibleButton = typedArray.getBoolean(R.styleable.HeaderView_headerButtonHidden, false)
+            if (visibleButton) button.visibility = INVISIBLE
+            else button.visibility = VISIBLE
 
             typedArray.recycle()
         }
