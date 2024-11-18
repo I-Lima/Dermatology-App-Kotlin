a4f67e9dab15b827bca2093874f3c643006044ee
Ingrid Lima <ibezerradelima@gmail.com>
2024-11-17 22:46:03 -0300

    :sparkles: feat: Add delete account
    #45

--- app/src/main/java/br/com/ilstudio/dermatologyapp/ui/SettingsActivity.kt
+++ app/src/main/java/br/com/ilstudio/dermatologyapp/ui/SettingsActivity.kt
@@ -2,11 +2,15 @@ package br.com.ilstudio.dermatologyapp.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import br.com.ilstudio.dermatologyapp.R
 import br.com.ilstudio.dermatologyapp.data.repository.UserRepository
 import br.com.ilstudio.dermatologyapp.databinding.ActivitySettingsBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.coroutines.launch
 
 class SettingsActivity : AppCompatActivity() {
@@ -27,20 +31,37 @@ class SettingsActivity : AppCompatActivity() {
 
         binding.itemNotification.setOnClickListener {}
         binding.itemAccount.setOnClickListener {
-            lifecycleScope.launch {
-                val result = userRepository.deleteAccount()
+            val view: View = layoutInflater.inflate(R.layout.view_bottom_sheet_delete_account, null)
+            val buttonCancel = view.findViewById<Button>(R.id.button_cancel)
+            val buttonDelete = view.findViewById<Button>(R.id.button_delete)
 
-                result.fold({
-                    Toast.makeText(baseContext, "Account deleted", Toast.LENGTH_SHORT).show()
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
 
-                    startActivity(Intent(baseContext, SplashScreenActivity::class.java))
-                    finish()
-                },{
-                    Toast.makeText(baseContext, it.message, Toast.LENGTH_SHORT).show()
-                })
+            buttonCancel.setOnClickListener {
+                dialog.dismiss()
             }
 
+            buttonDelete.setOnClickListener {
+                lifecycleScope.launch {
+                    deleteAccount()
+                }
+                dialog.dismiss()
+            }
         }
+    }
 
+    private suspend fun deleteAccount() {
+        val result = userRepository.deleteAccount()
+
+        result.fold({
+            Toast.makeText(baseContext, "Account deleted", Toast.LENGTH_SHORT).show()
+
+            startActivity(Intent(baseContext, LaunchScreenActivity::class.java))
+            finish()
+        },{
+            Toast.makeText(baseContext, it.message, Toast.LENGTH_SHORT).show()
+        })
     }
 }
