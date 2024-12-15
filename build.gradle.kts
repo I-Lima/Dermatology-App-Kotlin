c08a5a9d02f10ae3a5cb17855c4f0afa3bad1ef2
Ingrid Lima <ibezerradelima@gmail.com>
2024-12-15 15:32:18 -0300

    Remove all sonarcloud config
    #56

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,14 +16,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.3"
-    id("org.sonarqube") version "6.0.1.5171"
 }
-
-sonar {
-    properties {
-        property("sonar.projectKey", "I-Lima_Dermatology-App-Kotlin")
-        property("sonar.organization", "i-lima")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
-
