6f1b0ecc35807907644fb7a48c43b18a38b8c744
Ingrid Lima <ibezerradelima@gmail.com>
2025-01-29 21:04:53 -0300

    :construction: Doctors screen - Adapt to logic to get doctors data
    #60

--- app/src/main/java/br/com/ilstudio/dermatologyapp/ui/DoctorActivity.kt
+++ app/src/main/java/br/com/ilstudio/dermatologyapp/ui/DoctorActivity.kt
@@ -1,17 +1,47 @@
 package br.com.ilstudio.dermatologyapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.ilstudio.dermatologyapp.data.repository.FirestoreRepositoryDoctors
 import br.com.ilstudio.dermatologyapp.databinding.ActivityDoctorBinding
+import kotlinx.coroutines.launch
 
 class DoctorActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDoctorBinding
+    private lateinit var firestoreRepositoryDoctors: FirestoreRepositoryDoctors
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDoctorBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firestoreRepositoryDoctors = FirestoreRepositoryDoctors()
 
+        lifecycleScope.launch {
+            getAllDoctorsData()
+        }
+
+        binding.header.setOnBackButtonClickListener {
+            finish()
+        }
+    }
+
+    private suspend fun getAllDoctorsData() {
+        val response = firestoreRepositoryDoctors.getAll()
+
+        if (!response.success) {
+            binding.textError.text = response.message
+            binding.recycle.visibility = View.GONE
+            binding.error.visibility = View.VISIBLE
+        }
+
+        if (response.isEmpty) {
+            binding.recycle.visibility = View.GONE
+            binding.noData.visibility = View.VISIBLE
+        }
+
+        //binding.recycle.layoutManager = LinearLayoutManager(this)
     }
 }
