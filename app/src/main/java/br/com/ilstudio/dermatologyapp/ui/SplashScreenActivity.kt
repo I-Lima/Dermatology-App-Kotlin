ce12e29a131d9046500c1cb5d7530a58e331d444
Ingrid Lima <ibezerradelima@gmail.com>
2025-06-11 21:36:25 -0300

    :sparkles: feat: Add login validation on the splash screen
    When a user is logged in, redirect to the home screen. If not, redirect to the login screen.

--- app/src/main/java/br/com/ilstudio/dermatologyapp/ui/SplashScreenActivity.kt
+++ app/src/main/java/br/com/ilstudio/dermatologyapp/ui/SplashScreenActivity.kt
@@ -2,21 +2,26 @@ package br.com.ilstudio.dermatologyapp.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import br.com.ilstudio.dermatologyapp.data.service.FirebaseAuthService
 import br.com.ilstudio.dermatologyapp.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity: AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var firestoreServiceAuth: FirebaseAuthService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        firestoreServiceAuth = FirebaseAuthService(this)
         setContentView(binding.root)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, LaunchScreenActivity::class.java))
-            finish()
-        },3000)
+        val user = firestoreServiceAuth.getCurrentUser()
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            startActivity(Intent(this, LogInActivity::class.java))
+        }
+
+        finish()
     }
 }
